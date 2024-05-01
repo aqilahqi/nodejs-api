b373ccafd42494c74d0d7f1f476f25f81b5d6200
Aqilah <aqilahfatinfauzi@gmail.com>
2024-05-01 21:35:00 +0800

    add requirements in comments

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+/**
+ * Build a version controlled key-value store with a HTTP API we can query that from. The API
+needs to be able to:
+ *  Requirement 1: Accept {key: '...', value: '...'} and store them. If key exist, update the value
+ *  Requirement 2: Accept {key: '...'} and return it's corresponding value
+ *  Requirement 3: If give {key: '...', timestamp: '...'}, return the value with with the same timestamp
+ */
+
 const express = require("express");
 const mongoose = require("mongoose");
+const Store = require("./models/store.model");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node API");
-});
+app.use(express.json());
 
-app.post("/api/store", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+/**
+ * Requirement 1
+ */
+app.post("/api/store", async (req, res) => {
+  try {
+    const store = await Store.create(req.body);
+    res.status(200).json(store);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
