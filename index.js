f806286b22aec138bf9f0c728b9090f59a949442
Aqilah <aqilahfatinfauzi@gmail.com>
2024-05-01 20:58:10 +0800

    setup schema

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API");
 });
+
+app.post("/api/store", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://qilaqi:S5SKZhTO0BF3rK6P@backenddb.ldr8s5j.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection to database failed");
+  });
